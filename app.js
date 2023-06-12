9a1586228cb9145ec03e3debdc90908be9c91cdc
ehuntober <ehuntercrypto@gmail.com>
2023-06-12 16:12:09 +0000

    new file added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const shortid = require('shortid')
 const createHttpError = require('http-errors')
+const mongoose = require('mongoose')
+
 
 
 const app = express()
-
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000')
