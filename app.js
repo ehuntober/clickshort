2dec6f8cc3aa4a47157c6796feea4842a0ca2433
ehuntober <ehuntercrypto@gmail.com>
2023-06-12 16:19:22 +0000

    new file added

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.set('view engine', 'ejs')
 
-app.get('/', async(req,res,nex) =>{
-    res.
+app.get('/', async(req,res,next) =>{
+    res.render('index')
 })
 
 app.listen(3000, ()=>{
