bd1ba57971cfc57b459050963164b2e105013161
ehuntober <ehuntercrypto@gmail.com>
2023-06-12 16:15:30 +0000

    file

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express()
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.set('view engine', 'ejs')
+
+app.get('/', async(req,res,nex) =>{
+    res.
+})
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000')
